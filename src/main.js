a684c246b27282d5bc9e03499aa153de9c544eff
rezarinaldi <rez4rinaldi@gmail.com>
2024-06-08 16:45:22 +0700

    testing repo

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const Typed = require("typed.js");
+
+const typed = new Typed("#heading", {
+  strings: ["Hello, my name is Muhammad Fakhri Rizqullah", "I am a front-end developer", "I also do design"],
+  typeSpeed: 60,
+  loop: true,
+});
